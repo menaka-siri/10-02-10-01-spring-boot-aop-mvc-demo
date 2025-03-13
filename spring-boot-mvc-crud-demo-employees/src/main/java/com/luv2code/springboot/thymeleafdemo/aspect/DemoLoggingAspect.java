d8b5ecd7be6b796464e7361991781729f83999fd
Menaka S <menaka442@gmail.com>
2025-03-13 13:03:04 +0530

    add @AfterReturning aspect

--- spring-boot-mvc-crud-demo-employees/src/main/java/com/luv2code/springboot/thymeleafdemo/aspect/DemoLoggingAspect.java
+++ spring-boot-mvc-crud-demo-employees/src/main/java/com/luv2code/springboot/thymeleafdemo/aspect/DemoLoggingAspect.java
@@ -1,6 +1,7 @@
 package com.luv2code.springboot.thymeleafdemo.aspect;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
@@ -47,4 +48,18 @@ public class DemoLoggingAspect {
         }
 
     }
+
+    @AfterReturning(
+            pointcut = "forAppFlow()",
+            returning = "theResult")
+    public void afterReturning(JoinPoint theJoinPoint, Object theResult) {
+        // display method we are calling
+        String theMethod = theJoinPoint.getSignature().toShortString();
+        myLogger.info("====>> in @AfterReturning: calling method: " + theMethod);
+
+        // display data returned
+        myLogger.info("====>> result: " + theResult);
+    }
+
+
 }
